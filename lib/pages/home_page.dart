9b898ac5f34e02aa00bce5b0befd80a755f345ba
Jagddish <jagddish2@gmail.com>
2024-03-04 14:32:06 +0530

    Add pages

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,82 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:sankar/main.dart';
 import 'package:sankar/pages/login_page.dart';
+import 'package:sankar/text/applargetext.dart';
+import 'package:sankar/text/apptext.dart';
+import 'package:sankar/text/colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final imgs = [
+      'welcome-one.png',
+      'welcome-two.png',
+      'welcome-three.png',
+    ];
     final double height = MediaQuery.of(context).size.height;
     final space = SizedBox(height: height * 0.04);
+
     return Scaffold(
+      // backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Home"),
+        // backgroundColor: Colors.blue[50],
+        centerTitle: true,
       ),
-      body: Scaffold(
-        backgroundColor: Colors.pink.withOpacity(0.1),
-      ),
+      body: PageView.builder(
+          itemCount: 3,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (BuildContext, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                image: DecorationImage(
+                    image: AssetImage('images/' + imgs[index]),
+                    fit: BoxFit.cover),
+              ),
+              child: Container(
+                margin: EdgeInsets.only(top: 100.0, left: 40.0, right: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        LargeText(text: 'Hello'),
+                        AppText(
+                          text: "Mountains",
+                          size: 30.0,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      child: Column(
+                        children: List.generate(
+                          3,
+                          (indexDots) {
+                            return Container(
+                              margin: EdgeInsets.only(bottom: 3.0),
+                              height: index == indexDots ? 25.0 : 8,
+                              width: 8.0,
+                              decoration: BoxDecoration(
+                                  color: AppColors.mainColor,
+                                  borderRadius: BorderRadius.circular(10.0)),
+                            );
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
       drawer: Drawer(
         child: ListView(
           children: [
@@ -34,13 +94,13 @@ class HomePage extends StatelessWidget {
                   children: [
                     CircleAvatar(
                       backgroundImage: AssetImage('images/dp.jpg'),
-                      radius: height*0.05,
+                      radius: height * 0.05,
                     ),
                     SizedBox(
                       height: height * 0.04,
                     ),
                     Container(
-                      padding: EdgeInsets.only(bottom: height*0.02),
+                      padding: EdgeInsets.only(bottom: height * 0.02),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
@@ -48,7 +108,9 @@ class HomePage extends StatelessWidget {
                             'Shankar Ankepaka',
                             style: TextStyle(color: Colors.white),
                           ),
-                          SizedBox(height: height*0.01,),
+                          SizedBox(
+                            height: height * 0.01,
+                          ),
                           Text(
                             'Shankar@gmail.com',
                             style: TextStyle(color: Colors.white),
@@ -73,14 +135,20 @@ class HomePage extends StatelessWidget {
                   ),
                   TextButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomePage(),
+                          ));
                     },
-                    child: Text('Home',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.purple.shade800,
-                    ),),
+                    child: Text(
+                      'Home',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.purple.shade800,
+                      ),
+                    ),
                   )
                 ],
               ),
@@ -101,14 +169,20 @@ class HomePage extends StatelessWidget {
                   ),
                   TextButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomePage(),
+                          ));
                     },
-                    child: Text('Settings',
+                    child: Text(
+                      'Settings',
                       style: TextStyle(
                         fontSize: 20.0,
                         fontWeight: FontWeight.bold,
                         color: Colors.purple.shade800,
-                      ),),
+                      ),
+                    ),
                   ),
                 ],
               ),
@@ -129,14 +203,20 @@ class HomePage extends StatelessWidget {
                   ),
                   TextButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomePage(),
+                          ));
                     },
-                    child: Text('About us',
+                    child: Text(
+                      'About us',
                       style: TextStyle(
                         fontSize: 20.0,
                         fontWeight: FontWeight.bold,
                         color: Colors.purple.shade800,
-                      ),),
+                      ),
+                    ),
                   )
                 ],
               ),
@@ -157,14 +237,20 @@ class HomePage extends StatelessWidget {
                   ),
                   TextButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomePage(),
+                          ));
                     },
-                    child: Text('Contact us',
+                    child: Text(
+                      'Contact us',
                       style: TextStyle(
                         fontSize: 20.0,
                         fontWeight: FontWeight.bold,
                         color: Colors.purple.shade800,
-                      ),),
+                      ),
+                    ),
                   )
                 ],
               ),
@@ -185,14 +271,20 @@ class HomePage extends StatelessWidget {
                   ),
                   TextButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp(),));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MyApp(),
+                          ));
                     },
-                    child: Text('Log out',
+                    child: Text(
+                      'Log out',
                       style: TextStyle(
                         fontSize: 20.0,
                         fontWeight: FontWeight.bold,
                         color: Colors.purple.shade800,
-                      ),),
+                      ),
+                    ),
                   )
                 ],
               ),
